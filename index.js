6746b1c47d079c8d4ea4b8c4232b839d25bfeeef
Deniz Akşimşek <d.aksimsek1@outlook.com>
2018-12-13 17:24:59 +0200

    add pug rendering

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ console.log("[]Starting")
 const app = express()
 const server = app.listen(3000)
 
+app.set('views', './views')
+app.set('view engine', 'pug')
+
 app.get("/", (req, res) => 
-  res.sendFile(
-    __dirname+"/public/index.html"
+  res.render(
+    __dirname+"/views/index.html"
   )
 )
 
 app.get("/soru/", (req, res) => 
-  res.sendFile(
-    __dirname+"/public/soru.html"
+  res.render(
+    __dirname+"/views/soru.html",
+    {
+      soru: req.query.id,
+    }
   )
 )
