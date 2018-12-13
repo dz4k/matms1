aadee38c49c79f1e27d2bac820374c4460730b00
d.aksimsek1 <d.aksimsek1@outlook.com>
2018-12-13 15:15:42 -0500

    mmm

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const express = require("express")
 
 
-// TODO: everything
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+  // we're connected!
+})
+const msgSchema =  new mongoose.Schema({
+  yazan: String, 
+  icerik: String, 
+  cevaplar: [{yazan: String, icerik: String}] 
+}, {
+  timestamps: true
+} )
+const Msg = mongoose.model('Msg', msgSchema)
+
 
 console.log("[]Starting")
 const app = express()
 const server = app.listen(3000)
 
+app.use(express.static('wwwroot'))
 app.set('views', './views')
 app.set('view engine', 'pug')
 
 app.get("/", (req, res) => 
   res.render(
-    __dirname+"/views/index.html"
+    __dirname+"/views/index.pug"
   )
 )
 
 app.get("/soru/", (req, res) => 
   res.render(
-    __dirname+"/views/soru.html",
+    __dirname+"/views/soru.pug",
     {
       soru: req.query.id,
     }
   )
 )
+app.post("soru", (req, res) => {
+  
+} )
