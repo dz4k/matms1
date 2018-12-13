5d2b7db4f0729e9fdb7f7d18f652620eb5ccbf10
Deniz Akşimşek <d.aksimsek1@outlook.com>
2018-12-13 20:22:00 +0200

    o

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ const server = app.listen(3000)
 app.set('views', './views')
 app.set('view engine', 'pug')
 
+app.use(express.static("wwwroot"))
+
 app.get("/", (req, res) => 
   res.render(
-    __dirname+"/views/index.html"
+    __dirname+"/views/index.pug", {}
   )
 )
 
 app.get("/soru/", (req, res) => 
   res.render(
-    __dirname+"/views/soru.html",
+    __dirname+"/views/soru.pug",
     {
       soru: req.query.id,
     }
