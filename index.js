1903d7d880e4c86f854e3132a2b5c5ac595beb24
deniz-a <39595683+deniz-a@users.noreply.github.com>
2018-12-13 15:50:00 +0300

    Remove server side mathjax
    Math will be rendered on the client to reduce input latency & make use of MathJax screenreader support.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const mathjax = require("mathjax-node-svg2png")
 
+// TODO: everything
+
 console.log("[]Starting")
 const app = express()
 const server = app.listen(3000)
@@ -11,20 +13,8 @@ app.get("/", (req, res) =>
   )
 )
 
-function typeset(math, callback) {
-  mathjax.typeset({
-     math: math,
-     format: "AsciiMath",
-     png: true,
-    scale: 5
-   }).then(data => callback(toImg(data.png)))
-}
-
-function toImg(a) {
-  return "<img src=\""+a+"\">"
-
-}
-
-app.get("/render/", (req, res) => {
-  typeset(req.query.math, a =>res.send(a))
-})
+app.get("/soru/", (req, res) => 
+  res.sendFile(
+    __dirname+"/public/soru.html"
+  )
+)
